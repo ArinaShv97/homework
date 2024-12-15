c19ec6bba835bfad1fa15abb6e944d2a73763f2f
ArinaShv97 <arina.kvochko@gmail.com>
2024-12-15 23:51:03 +0500

    aimÐ

---
+++ additional_task.py
@@ -0,0 +1,3 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+my_dict = {'Johnny':5, 3,3, 5, 4 , 'Bilbo':2, 2, 2, 3, 'Steve': 4, 5, 5, 2, 'Khendrik':4, 4, 3, 'Aaron':5, 5, 5, 4, 5}
