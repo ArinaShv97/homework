cb3d8b7996a1182feb7a5ee0b00bfcfe1f07dade
ArinaShv97 <arina.kvochko@gmail.com>
2024-12-21 00:56:00 +0500

    ÐисисправлеенноÐÐ

--- module_2/while.py
+++ module_2/while.py
@@ -1,23 +1,13 @@
 my_list =  [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-i =0
-new_list = []
-while len(my_list) > i:
+i = 0
+
+while i < len(my_list):
     if my_list[i] < 0 :
-        print('значение отрицательное')
-        continue
-    new_list.append(my_list[i])
-    i = + 1
-    print(new_list)
-#Не могу понять в чем ошибка, выводит список только 42,69 и до конца 69
+        break
 
-
-
-
-
-
-
-
-
+    if my_list[i] > 0:
+            print(my_list[i])
+    i += 1
 
 
 
